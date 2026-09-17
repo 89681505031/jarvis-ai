18618a5e3a7e4cc311424ae6052b925942060c24
Alex <alexpimenov98gg@gmail.com>
2026-09-18 05:08:25 +0900

    Add Jarvis Phone Android module

---
+++ mobile/android/app/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.jarvis.phone"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.jarvis.phone"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
+
